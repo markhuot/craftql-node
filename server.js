fb791fa63dee0e0d3230d2058d5f0521973394d3
Mark Huot <mark@markhuot.com>
2018-02-04 07:46:01 -0500

    got abstraction working and HOC

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const express = require('express')
 const app = express()
-const webpack = require('webpack')
-const compiler = webpack(require('./webpack.config'))
-const middleware = require('webpack-dev-middleware')(compiler, {serverSideRender: true})
-const requireFromString = require('require-from-string')
-const ReactDOMServer = require('react-dom/server')
-const createElement = require('react').createElement
+const craftql = require('./lib/craftql')
 
-app.use(middleware)
+craftql.addHigherOrderComponent(require('./lib/TestHOC'))
 
-app.get('*', (req, res) => {
-    const url = req.url === '/' ? 'index' : req.url.replace(/^\//, '')
-    const bundle = middleware.context.fs.readFileSync(`/${url}.js`)
-    let Component = requireFromString(bundle.toString('utf8'))
-    Component = Component.default || Component
-    res.send(ReactDOMServer.renderToString(createElement(Component, null)))
-})
+app.use(express.static('./public'))
+app.use(craftql.routing)
+app.use(craftql.middleware)
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () =>
+    console.log('Example app listening on port 3000!'))
